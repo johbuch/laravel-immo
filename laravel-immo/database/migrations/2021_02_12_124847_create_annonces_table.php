8804b8a2957cc12773d950cf5819fe579723ad73
johbuch <johan.buchert@gmail.com>
2021-02-12 14:00:32 +0100

    1ere migration en bdd

---
+++ laravel-immo/database/migrations/2021_02_12_124847_create_annonces_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnoncesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('annonces', function (Blueprint $table) {
+            $table->id();
+            $table->string('ref_annonce');
+            $table->float('pix_annonce');
+            $table->float('surface_habitable');
+            $table->integer('nombre_de_piece');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('annonces');
+    }
+}
