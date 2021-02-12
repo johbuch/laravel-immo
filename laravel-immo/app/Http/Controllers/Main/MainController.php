90961b3dcb1ce740cf1749b8bc22e9ecc506860f
johbuch <johan.buchert@gmail.com>
2021-02-12 15:17:05 +0100

    update

--- laravel-immo/app/Http/Controllers/Main/MainController.php
+++ laravel-immo/app/Http/Controllers/Main/MainController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
+use App\Models\Annonce;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function browse() {
-        return view('main.browse');
+        $annonces = Annonce::all();
+        return view('main.browse', compact('annonces'));
     }
 }
--- laravel-immo/resources/views/main/browse.blade.php
+++ laravel-immo/resources/views/main/browse.blade.php
@@ -4,17 +4,19 @@
     <div class="container">
         <h2 class="text-center">Découvrez toutes nos annonces</h2>
         <div class="row">
+            @foreach($annonces as $annonce)
                 <div class="col-4">
                     <div class="card border-info mb-3">
-                        <img class="card-img-top" src="" alt="Card image cap">
+                        <img class="card-img-top" src="https://picsum.photos/200" alt="Card image cap">
                         <div class="card-body">
-                            <div>Ref annonce</div>
-                            <div>Prix</div>
-                            <div>Surface habitable</div>
-                            <div>Nombre de pièces</div>
+                            <div>Référence : {{ $annonce->ref_annonce }}</div>
+                            <div>Prix : {{ $annonce->prix_annonce }} €</div>
+                            <div>Surface habitable : {{ $annonce->surface_habitable }}m²</div>
+                            <div>Nombre de pièces: {{ $annonce->nombre_de_piece }}</div>
                         </div>
                     </div>
                 </div>
+            @endforeach
         </div>
     </div>
 @endsection
